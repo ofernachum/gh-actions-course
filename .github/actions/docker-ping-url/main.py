1c7c1c40e8433e05153fa1baa2d59199f23f6d66
Ofer Nachum <ofernachum@gmail.com>
2025-06-10 14:46:24 +0300

    Feat(17-2-Docker): Add Output

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -2,6 +2,14 @@ import os
 import requests
 import time
 
+# This function append a key value line to a file.
+# It is later used to append an Output key=value line to the $GITHUB_OUTPUT file below (C45)
+def set_output(file_path, key, value):
+    with open(file_path, 'a') as file:
+        print(f'{key}={value}', file=file)
+        
+
+
 def ping_url(url, delay, max_trails):
     trails = 0
     
@@ -33,6 +41,12 @@ def run():
     max_trails = int(os.getenv('INPUT_MAX_TRAILS'))
     
     website_reachable = ping_url(website_url, delay, max_trails)
+    
+    # Using the set_output function declared above (C45) to add a line
+    # to $GITHUB_OUTPUT file: url-reachable=false/true
+    # The key must match the Output defined in the outputs block in action.yaml !
+    set_output(os.getenv('GITHUB_OUTPUT'), 'url-reachable', website_reachable)
+    
     
     if not website_reachable:
         raise Exception (f"Website {website_url} is malformed or unreachable.")
